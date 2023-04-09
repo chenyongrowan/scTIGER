bc1066d2b4ccd2541f1a067e612cad52efb935fd
chenyongrowan <83730164+chenyongrowan@users.noreply.github.com>
2023-04-09 16:19:29 -0400

    Update runExample.py

--- Example/runExample.py
+++ Example/runExample.py
@@ -3,4 +3,4 @@
 import os
 
 os.chdir(..)
-os.system(./runSIDELINE.py -goi AR+PTEN+ERG -ctrl ./Example/Data/Patient4_Benign_endothelial.csv -exp ./Example/Data/Patient4_Tumor_endothelial.csv -p 50 -top 100 -zero 0.15 -o SIDELINE_SampleResult_ProstateCancer)
+os.system(./runSIDELINE.py -goi AR+PTEN+ERG -ctrl ./Example/Data/Patient4_Benign_endothelial.csv -exp ./Example/Data/Patient4_Tumor_endothelial.csv -p 50 -top 100 -zero 0.15 -o ./Example/SIDELINE_SampleResult_ProstateCancer)
