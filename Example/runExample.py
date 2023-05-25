111b58c1ed55d48bac065064fe073240e4515f5b
chenyongrowan <83730164+chenyongrowan@users.noreply.github.com>
2023-05-25 13:23:19 -0400

    Update runExample.py

--- Example/runExample.py
+++ Example/runExample.py
@@ -2,5 +2,4 @@
 
 import os
 
-os.chdir(..)
-os.system(./run_scTIGER.py -goi AR+PTEN+ERG -ctrl ./Example/Data/Patient4_Benign_endothelial.csv -exp ./Example/Data/Patient4_Tumor_endothelial.csv -p 50 -top 100 -zero 0.15 -o ./Example/SampleResult_ProstateCancer)
+os.system('./run_scTIGER.py -goi AR+PTEN+ERG -ctrl ./Example/Data/Patient4_Benign_endothelial.csv -exp ./Example/Data/Patient4_Tumor_endothelial.csv -p 50 -top 100 -zero 0.15 -o ./Example/SampleResult_ProstateCancer')
